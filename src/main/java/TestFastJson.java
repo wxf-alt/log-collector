13c18d649cb6ff31a62113dfc3d3790101128492
wxf <wxf18845574579@163.com>
2022-12-02 16:09:30 +0800

    Initial commit

---
+++ src/main/java/TestFastJson.java
@@ -0,0 +1,13 @@
+/**
+ * @Auther: wxf
+ * @Date: 2022/12/2 16:07:44
+ * @Description: TestFastJson
+ * @Version 1.0.0
+ */
+public class TestFastJson {
+    public static void main(String[] args) {
+
+
+
+    }
+}
